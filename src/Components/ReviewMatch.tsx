d72b284ba0b94c9b0ba139ef3db846e3e2ff8cca
ramirolynch <ramirolynch@yahoo.com>
2022-04-27 10:13:02 -0500

    added toastify to ReviewMatch

--- src/Components/ReviewMatch.tsx
+++ src/Components/ReviewMatch.tsx
@@ -1,6 +1,8 @@
 import { useContext, useState } from "react";
 import { ScreeningContext } from "../Context/ScreeningContext";
 import { postReview } from "../Services/ScreeningApi";
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 export function ReviewMatch (props: {elem:any, matchscore:number}) {
 
@@ -20,15 +22,23 @@ export function ReviewMatch (props: {elem:any, matchscore:number}) {
     }
 
 
-
-
     function handleClick() {
         
         console.log('positiveMatch', positiveMatch);
         console.log('matchedName', props.elem.name)
         console.log('userId', user_id);
 
-        postReview(props.elem.id ,searched_name, props.elem.name, props.matchscore, positiveMatch, reviewComment, user_id)
+        postReview(props.elem.id, searched_name, props.elem.name, props.matchscore, positiveMatch, reviewComment, user_id).then(() => {
+            toast.success('Review Saved to DB', {
+                position: "top-center",
+                autoClose: 5000,
+                hideProgressBar: false,
+                closeOnClick: true,
+                pauseOnHover: true,
+                draggable: true,
+                progress: undefined,
+                });
+        })
     }
 
     return(
@@ -52,6 +62,18 @@ export function ReviewMatch (props: {elem:any, matchscore:number}) {
         </div>
             
             <button onClick={handleClick}>Submit Review</button>
+
+        <ToastContainer
+        position="top-center"
+        autoClose={1000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        />
         
         </div>
     );
